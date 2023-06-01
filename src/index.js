831630b881ecb592a04630d272da17517532e4ec
Daniil Panov <from4life@pm.me>
2023-06-01 20:04:20 +0700

    Add a function to change the theme according to the weather, add mediaquery

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log('weathaaaaaa')
-
+// make an object with all the necessary information
 const getWeatherInfo = (prom) => {
+    console.log(prom)
     const location = prom.location.name;
     const tempC = `${prom.current.temp_c}°C`;
     const feelsTempC = `${prom.current.feelslike_c}°C`;
@@ -11,10 +12,11 @@ const getWeatherInfo = (prom) => {
     const windKph = `${prom.current.wind_kph} km/h`;
     const windMph = `${prom.current.wind_mph} m/h`;
     const weatherIcon = prom.current.condition.icon;
+    const weatherCode = prom.current.condition.code;
 
-    return {location, tempC, feelsTempC, tempF, feelsTempF, weatherDescription, humidity, windKph, windMph, weatherIcon};
+    return {location, tempC, feelsTempC, tempF, feelsTempF, weatherDescription, humidity, windKph, windMph, weatherIcon, weatherCode};
 }
-
+// display the information about the weather
 const displayWeather = (obj) => {
     const cityName = document.querySelector('.city-name');
     const tempReal = document.querySelector('.temp-real');
@@ -30,18 +32,48 @@ const displayWeather = (obj) => {
     weatherIcon.src = obj.weatherIcon;
     tempFeel.innerText = obj.feelsTempC;
     weatherHumidity.innerText = obj.humidity;
-    weatherWind.innerText = obj.windKph;    
+    weatherWind.innerText = obj.windKph;
 }
-
+// change color theme depending on weather
+const changeColorTheme = (obj) => {
+    const websiteTheme = document.documentElement;
+    if (obj.weatherCode === 1000) {
+        websiteTheme.style.setProperty('--header-n-text', '#146152');
+        websiteTheme.style.setProperty('--text-in-header-btn', '#FFEC5C');
+        websiteTheme.style.setProperty('--body-background', '#B4CF66');
+        websiteTheme.style.setProperty('--search-border', '2px solid #347355');
+        websiteTheme.style.setProperty('--weather-card', '#FFEC5C');
+    } else {
+        websiteTheme.style.setProperty('--header-n-text', '#364559');
+        websiteTheme.style.setProperty('--text-in-header-btn', '#CEDEF2');
+        websiteTheme.style.setProperty('--body-background', '#566D8C');
+        websiteTheme.style.setProperty('--search-border', '2px solid #566D8C');
+        websiteTheme.style.setProperty('--weather-card', '#9CB6D9');
+    }
+}
+// fetch the weather from WeatherAPI
 const getWeather = async (city) => {
-    const result = await fetch(`https://api.weatherapi.com/v1/current.json?key=6906dfa564e940bfbc864752232105&q=${city.replace(/ /g, '\\')}`, {mode: 'cors'});
-    const weather = await result.json();
-    const cityWeather = getWeatherInfo(weather);
-    displayWeather(cityWeather);  
+    try {
+        const result = await fetch(`https://api.weatherapi.com/v1/current.json?key=6906dfa564e940bfbc864752232105&q=${city.replace(/ /g, '\\')}`, {mode: 'cors'});
+        const weather = await result.json();
+        const cityWeather = getWeatherInfo(weather);
+        displayWeather(cityWeather);
+        changeColorTheme(cityWeather);
+    } catch (error) {
+        alert('Please, enter the city name!');
+    }
 }
 
+// make the button work
 const searchBtn = document.querySelector('.search-btn');
 searchBtn.addEventListener('click', () => {
     const searchValue = document.querySelector('#city').value;
     getWeather(searchValue);
+})
+// also search on 'Enter'
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        const searchValue = document.querySelector('#city').value;
+        getWeather(searchValue);
+    }
 })
